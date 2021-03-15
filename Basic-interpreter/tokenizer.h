1f78e3aacf4e98c400310945bc1eb481f93a658f
KendrickChou <59338412+KendrickChou@users.noreply.github.com>
2021-03-15 19:59:22 +0800

    Add files via upload

--- Basic-interpreter/tokenizer.h
+++ Basic-interpreter/tokenizer.h
@@ -3,22 +3,53 @@
 
 #include <string>
 
-//class tokenizer
-//{
-//public:
-//   struct node{
-//        std::string token;
-//        node *pre,*succ;
-//    }
+enum OPERATION{ADD,SUB,MUL,DIV,EXP,LP,RP,EQ};
 
-//   tokenizer();
-//   node *getHead();
-//   int getLength();
-//   void transStringtoList(const std::string &text);
-//   node *getNodeI(int I);
-//private:
-//   node *head;
-//   int length;
-//};
+enum STATMENT{REM,LET,PRINT,INPUT,GOTO,IF,THEN,END};
+
+enum COMMAND{RUN,LOAD,LIST,CLEAR,HELP,QUIT};
+
+/*
+ * ln: line number,
+ * op: operation
+ * var: variable
+ * num: number
+ * stat: statment
+ * com: command
+*/
+enum TYPE{ln,op,var,num,stat,com};
+
+class node
+{
+private:
+    TYPE type;
+    union Data{
+        int LN = 0;
+        OPERATION Op;
+        std::string Var;
+        int Num;
+        std::string Stat;
+        std::string Comm;
+    };
+    Data val;
+public:
+    void setNode(std::string &s);
+    node *pre,*succ;
+};
+
+class tokenizer
+{
+public:
+
+
+   tokenizer();
+   node *getHead();
+   int getLength();
+   void transStringtoList(const std::string &text);
+   node *getNodeI(int I);
+private:
+   node *head;
+   int length;
+};
 
 #endif // TOKENIZER_H
